04017a2f947c22aaad4dbd022b434cdb057e53de
Ryan Davis <ryepup@gmail.com>
2017-03-11 18:45:17 -0500

    Stop littering

---
+++ load.d/autosave.el
@@ -0,0 +1,6 @@
+;; autosave / backup files in the system temp folder
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory))
+      auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
